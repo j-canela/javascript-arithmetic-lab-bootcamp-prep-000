53d4fa4088c24927784a89c73a7010c391f3ed5c
jemsley canela <jemscanela@gmail.com>
2018-04-05 16:54:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,14 +14,19 @@ function divide(a,b){
   return a/b
 }
 
-function inc(n)(
-  )
+function inc(n){
+ return n++
+}
   
   function dec(n){
-    
+  return  n--
   }
   
   function makeInt(n){
-    
+    return parseInt(n,10)
+  }
+  
+  function preserveDecimal(n){
+    return parse
   }
   
